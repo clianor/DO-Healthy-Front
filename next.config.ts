81d1ebc5eb6109fd8e5bd08bdd73e28f277956ed
clianor <ejrgus94160@gmail.com>
2024-12-29 23:51:11 +0900

    build: 프로덕션 빌드 시 data-testid 속성 제거 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  compiler: {
+    reactRemoveProperties: { properties: ['^data-testid$'] },
+  },
 };
 
 export default nextConfig;
