ce935fe06fac8cf1d53a12524c81dfaa713655ef
clianor <ejrgus94160@gmail.com>
2025-01-14 18:32:52 +0900

    feat: 헤더 반응형 구현
    - 모바일 환경에서 Sheet 컴포넌트를 이용한 메뉴 구현
    - 네비게이션 설정 분리
    - 컨테이너 컴포넌트 적용

---
+++ src/widgets/header/header.config.ts
@@ -0,0 +1,6 @@
+export const NAVIGATIONS = [
+  { href: '/', text: '홈' },
+  { href: '/recipes', text: '레시피' },
+  { href: '/milfap', text: '밀프랩' },
+  { href: '/recipes/disease', text: '질환별 식단' },
+];
