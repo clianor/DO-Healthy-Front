c01a5b972a2c64070c08861494daa1971a38024e
clianor <ejrgus94160@gmail.com>
2025-01-14 18:32:52 +0900

    feat: 헤더 반응형 구현
    - 모바일 환경에서 Sheet 컴포넌트를 이용한 메뉴 구현
    - 네비게이션 설정 분리
    - 컨테이너 컴포넌트 적용

--- src/widgets/header/header.ui.tsx
+++ src/widgets/header/header.ui.tsx
@@ -1,3 +1,4 @@
+import { Menu } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -5,40 +6,77 @@ import { PropsWithChildren } from 'react';
 
 import { useSessionStore } from '@/shared/session';
 import { Button } from '@/shared/ui/button';
+import { Container } from '@/shared/ui/container';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/shared/ui/sheet';
 import { cn } from '@/shared/utils';
+
+import { NAVIGATIONS } from './header.config';
 
 export function Header() {
   const isLoggedIn = useSessionStore((state) => !!state.session);
 
   return (
-    <header className="flex justify-between border-b border-b-border px-8 py-4">
-      <nav>
-        <div className="flex items-center gap-x-8">
-          <Link href="/" aria-label="홈으로 이동">
-            <Image className="invert" src="/vercel.svg" alt="로고" width={32} height={32} />
-          </Link>
-          <ul className="flex list-none items-center gap-x-8">
-            {[
-              { href: '/', text: '홈' },
-              { href: '/recipes', text: '레시피' },
-              { href: '/milfap', text: '밀프랩' },
-              { href: '/recipes/disease', text: '질환별 식단' },
-            ].map(({ href, text }) => (
-              <li key={href}>
-                <NavLink href={href}>{text}</NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
+    <header className="border-b border-b-border px-8 py-4">
+      <Container>
+        <nav className="flex justify-between">
+          <div className="flex items-center gap-x-8">
+            <Link href="/" aria-label="홈으로 이동">
+              <Image className="invert" src="/vercel.svg" alt="" width={32} height={32} />
+            </Link>
+            <ul className="hidden list-none items-center gap-x-8 lg:flex">
+              {NAVIGATIONS.map(({ href, text }) => (
+                <li key={href}>
+                  <NavLink href={href}>{text}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-      <div>
-        <Button asChild>
-          <Link href={isLoggedIn ? '/auth/logout' : '/auth/login'}>
-            {isLoggedIn ? '로그아웃' : '로그인'}
-          </Link>
-        </Button>
-      </div>
+          <div className="hidden lg:flex">
+            <Button asChild>
+              <Link href={isLoggedIn ? '/auth/logout' : '/auth/login'}>
+                {isLoggedIn ? '로그아웃' : '로그인'}
+              </Link>
+            </Button>
+          </div>
+
+          <div className="flex lg:hidden">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button size="icon">
+                  <span className="sr-only">메뉴 오픈</span>
+                  <Menu aria-hidden />
+                </Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>
+                    <Link href="/" aria-label="홈으로 이동">
+                      <Image className="invert" src="/vercel.svg" alt="" width={32} height={32} />
+                    </Link>
+                  </SheetTitle>
+                  <SheetDescription>
+                    <ul className="mt-8 flex list-none flex-col items-start gap-y-4">
+                      {NAVIGATIONS.map(({ href, text }) => (
+                        <li key={href}>
+                          <NavLink href={href}>{text}</NavLink>
+                        </li>
+                      ))}
+                    </ul>
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </nav>
+      </Container>
     </header>
   );
 }
