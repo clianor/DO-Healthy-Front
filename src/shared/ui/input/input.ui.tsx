9c7452934f3c04aa39d923128e49882929f9745a
clianor <ejrgus94160@gmail.com>
2025-01-15 20:46:42 +0900

    feat: Input 컴포넌트에 size 바리언트 추가
    - class-variance-authority를 사용하여 size 바리언트 추가 (default, sm, lg)
    - input 스타일에 number type spinner 제거 스타일 추가

--- src/shared/ui/input/input.ui.tsx
+++ src/shared/ui/input/input.ui.tsx
@@ -1,20 +1,28 @@
+import { VariantProps, cva } from 'class-variance-authority';
 import * as React from 'react';
 
 import { cn } from '@/shared/utils';
 
-type InputProps = React.ComponentProps<'input'>;
+const inputVariants = cva(
+  'flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
+  {
+    variants: {
+      size: {
+        default: 'h-9 px-3 py-1',
+        sm: 'h-8 px-3 py-1 text-xs',
+        lg: 'h-10 px-4 py-2',
+      },
+    },
+    defaultVariants: {
+      size: 'default',
+    },
+  },
+);
 
-function Input({ className, type, ...props }: InputProps) {
-  return (
-    <input
-      type={type}
-      className={cn(
-        'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-        className,
-      )}
-      {...props}
-    />
-  );
+type InputProps = Omit<React.ComponentProps<'input'>, 'size'> & VariantProps<typeof inputVariants>;
+
+function Input({ className, type, size, ...props }: InputProps) {
+  return <input type={type} className={cn(inputVariants({ size }), className)} {...props} />;
 }
 
 export { Input };
