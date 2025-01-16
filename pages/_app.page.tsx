590083f3afc33324216438603d33d09f87b19309
clianor <ejrgus94160@gmail.com>
2025-01-16 21:56:14 +0900

    feat: 레이아웃 컴포넌트 추가 및 적용
    - 공통 레이아웃 컴포넌트 추가
    - Header와 Footer를 레이아웃으로 이동하여 중복 제거

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app';
 
+import { Layout } from '@/pages/layouts';
+
 import './styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
