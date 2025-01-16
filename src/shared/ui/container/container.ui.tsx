e016251c998559d8cb0aaf58ee211b8933a260a4
clianor <ejrgus94160@gmail.com>
2025-01-16 21:54:15 +0900

    style: Container 컴포넌트에 w-full 클래스 추가

--- src/shared/ui/container/container.ui.tsx
+++ src/shared/ui/container/container.ui.tsx
@@ -6,7 +6,7 @@ type ContainerProps = ComponentProps<'div'>;
 
 export function Container({ children, className, ...props }: ContainerProps) {
   return (
-    <div className={cn('mx-auto max-w-screen-2xl px-8', className)} {...props}>
+    <div className={cn('mx-auto w-full max-w-screen-2xl px-8', className)} {...props}>
       {children}
     </div>
   );
